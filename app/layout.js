e8e40db5dec441ab07c35362749a70938a375fbc
Hubert Grzesiak <vTenn3R@wp.pl>
2023-08-20 13:59:11 +0200

    zmiana metadaty

--- app/layout.js
+++ app/layout.js
@@ -5,12 +5,13 @@ import "@styles/Post.css";
 import Head from "next/head";
 import Navbar from "@components/Navbar.jsx";
 export const metadata = {
-  title: "Hubert Grzesiak",
-  description: "My Portfolio",
+  title: "Hubert Grzesiak - Frontend Developer",
+  description:
+    "Welcome to my website! My name is Hubert Grzesiak, and I'm a passionate Frontend Developer. I specialize in technologies like React.js, Tailwind CSS, Next.js, and MongoDB. Explore my portfolio to see my projects and skills in action.",
 };
 
 const RootLayout = ({ children }) => (
-  <html lang="en">
+  <html lang="pl">
     <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
