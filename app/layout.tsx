210019ee920eb0fe1f13f130692872ff0846b623
Hubert Grzesiak <vTenn3R@wp.pl>
2024-08-15 16:03:55 +0200

    seo upgraded

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,35 @@ import "@styles/Navbar.css";
 import "@styles/globals.css";
 import "@styles/Animation.scss";
 import "@styles/Post.css";
-
+import type { Metadata } from "next";
 import Head from "next/head";
 import Navbar from "@components/Navbar";
-export const metadata = {
+
+export const metadata: Metadata = {
   title: "Hubert Grzesiak - Frontend Developer",
   description:
     "Welcome to my website! My name is Hubert Grzesiak, and I'm a passionate Frontend Developer. I specialize in technologies like React.js, Tailwind CSS, Next.js, and MongoDB. Explore my portfolio to see my projects and skills in action.",
+  twitter: {
+    card: "summary_large_image",
+    site: "@HubertGrzesiak",
+  },
+  alternates: {
+    canonical: "https://hubertgrzesiak.com",
+  },
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://hubertgrzesiak.com",
+    siteName: "Hubert Grzesiak",
+    images: [
+      {
+        url: "https://hubertgrzesiak.com/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Hubert Grzesiak - Frontend Developer",
+      },
+    ],
+  },
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
