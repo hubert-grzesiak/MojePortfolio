8dad88c8a05bb5fe9f366d7d0808138480b98119
Hubert Grzesiak <vTenn3R@wp.pl>
2024-08-15 16:07:44 +0200

    seo upgraded

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import "@styles/globals.css";
 import "@styles/Animation.scss";
 import "@styles/Post.css";
 import type { Metadata } from "next";
-import Head from "next/head";
 import Navbar from "@components/Navbar";
+import { Inter } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Hubert Grzesiak - Frontend Developer",
@@ -18,6 +18,9 @@ export const metadata: Metadata = {
     canonical: "https://hubertgrzesiak.com",
   },
   openGraph: {
+    title: "Hubert Grzesiak - Frontend Developer",
+    description:
+      "Welcome to my website! My name is Hubert Grzesiak, and I'm a passionate Frontend Developer. I specialize in technologies like React.js, Tailwind CSS, Next.js, and MongoDB. Explore my portfolio to see my projects and skills in action.",
     type: "website",
     locale: "en_US",
     url: "https://hubertgrzesiak.com",
@@ -32,18 +35,11 @@ export const metadata: Metadata = {
     ],
   },
 };
+const inter = Inter({ subsets: ["latin"] });
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html lang="pl">
-    <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap"
-        rel="stylesheet"
-      />
-    </Head>
-    <body>
+    <body className={inter.className}>
       <div className="main">
         <div className="gradient" />
       </div>
