8a9884ad6432c03a0519dab07003598c898401e5
Hubert Grzesiak <hubert@MacBook-Pro-Hubert.local>
2024-08-20 08:31:28 +0200

    accesibility v3

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -21,7 +21,10 @@ const Slider = React.forwardRef<
     <SliderPrimitive.Track className="relative h-[2px] w-full grow overflow-hidden rounded-full bg-[#151E2C66] dark:bg-black-400">
       <SliderPrimitive.Range className="absolute h-full bg-[#151E2C66] dark:bg-black-400" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-[30px] w-[30px] rounded-full border-2 border-primary bg-black-200 ring-offset-background transition-colors hover:ring-1 hover:ring-[#151E2C66] hover:ring-offset-[8px] hover:ring-offset-white-800 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-[8px] disabled:pointer-events-none disabled:opacity-50 dark:bg-white-900 dark:hover:ring-black-400 dark:hover:ring-offset-black-200 lg:h-11 lg:w-11 lg:hover:ring-offset-[13px] lg:focus-visible:ring-offset-[13px]" />
+    <SliderPrimitive.Thumb
+      aria-label="thumb"
+      className="block h-[30px] w-[30px] rounded-full border-2 border-primary bg-black-200 ring-offset-background transition-colors hover:ring-1 hover:ring-[#151E2C66] hover:ring-offset-[8px] hover:ring-offset-white-800 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-[8px] disabled:pointer-events-none disabled:opacity-50 dark:bg-white-900 dark:hover:ring-black-400 dark:hover:ring-offset-black-200 lg:h-11 lg:w-11 lg:hover:ring-offset-[13px] lg:focus-visible:ring-offset-[13px]"
+    />
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
