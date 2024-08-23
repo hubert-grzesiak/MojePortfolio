2dc61bc4b1d6bea20e260980fdcfed5115efb7fb
Hubert Grzesiak <hubert@MacBook-Pro-Hubert.local>
2024-08-23 23:35:00 +0200

    comment toc

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -7,7 +7,7 @@ import { Metadata } from "next";
 import { siteConfig } from "@/config/site";
 import { Tag } from "@components/blog/Tag";
 import { calculateReadingTime } from "@/lib/utils";
-import { ClientSideTableOfContents } from "./ClientSideTableOfContents";
+// import { ClientSideTableOfContents } from "./ClientSideTableOfContents";
 
 interface PostPageProps {
   params: {
@@ -95,13 +95,13 @@ export default async function PostPage({ params }: PostPageProps) {
         <hr className="my-4" />
         <MDXContent code={post.body} />
       </article>
-      <aside className="sticky">
+      {/* <aside className="sticky">
         <div className="sticky top-16 hidden pb-6 text-sm xl:block">
           <div className="sticky top-16 -mt-10 max-h-[calc(var(--vh)-4rem)] overflow-y-auto pt-10">
             <ClientSideTableOfContents />
           </div>
         </div>
-      </aside>
+      </aside> */}
     </main>
   );
 }
