feb9e68b2ac05bc32e370e574eba04ff25133015
Hubert Grzesiak <hubert@MacBook-Pro-Hubert.local>
2024-08-25 00:39:55 +0200

    changes to resume

--- components/home/ResumeButton.tsx
+++ components/home/ResumeButton.tsx
@@ -2,25 +2,51 @@
 
 import Typography from "@components/shared/Typography";
 import { DownloadIcon } from "../icons";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const ResumeButton = () => {
   return (
     <Typography
       as="div"
-      className="text-black-200 dark:text-white-900"
+      className="flex gap-[3px] text-black-200 dark:text-white-900"
       variant={"small-regular"}
     >
-      <a
-        className="flex w-full items-center gap-[3px]"
-        href="/CV_EN_Hubert_Grzesiak.pdf"
-        target="_blank"
-      >
-        <DownloadIcon
-          onClick={() => {}}
-          className="h-[21px] w-[21px] shrink-0 hover:cursor-pointer dark:[&>path]:fill-white-900"
-        />
-        <span>Resume</span>
-      </a>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <a
+              href="/RESUME_EN_Hubert_Grzesiak.pdf"
+              download="RESUME_Hubert_Grzesiak.pdf"
+            >
+              <DownloadIcon className="h-[21px] w-[21px] shrink-0 hover:cursor-pointer dark:[&>path]:fill-white-900" />
+            </a>
+          </TooltipTrigger>
+          <TooltipContent side={"bottom"}>
+            <span>Download Resume</span>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <a
+              className="flex w-full items-center gap-[3px]"
+              href="/RESUME_EN_Hubert_Grzesiak.pdf"
+              target="_blank"
+            >
+              <span>Resume</span>
+            </a>
+          </TooltipTrigger>
+          <TooltipContent side={"bottom"}>
+            <span>Open Resume in new tab</span>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </Typography>
   );
 };
